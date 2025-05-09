098cf9eb35bcca56314547bc334b7973835bbdf8
JFLigroup <153267627+JFLigroup@users.noreply.github.com>
2025-05-09 16:34:21 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../chem_topo'))
+sys.path.insert(0, os.path.abspath('../..'))
 project = 'chem_topo'
 copyright = '2025, zheng shisheng , zhang ximing , liu hengshu , liang gehao'
 author = 'zheng shisheng , zhang ximing , liu hengshu , liang gehao'
